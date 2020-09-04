1c8ce8ee52e57b6eedf631cafff9ebb02c112729
yu-chinghsu <yuching.hsu@utah.edu>
2020-09-03 22:38:46 -0600

    http and https both work

--- webbrowser.py
+++ webbrowser.py
@@ -8,47 +8,71 @@ class Url:
     host: str
     port: int
     path: str
+    def __init__(self, scheme, host, port, path):
+        self.scheme = scheme
+        self.host = host
+        self.port = port
+        self.path = path
+
     def __str__(self) -> str:
         return f"[Scheme: {self.scheme}]\n[Host: {self.host}]\n[port: {self.port}]\n[path: {self.path}]"
 
-def request(url):
+class Response:
+    http_version: str
+    status: int
+    explanation: str
+    headers: {}
+    body: str
+    def __init__(self, http_version, status, explanation, headers, body):
+        self.http_version = http_version
+        self.status = status
+        self.explanation = explanation
+        self.headers = headers
+        self.body = body
+
+    def __str__(self) -> str:
+        output = f"[Version: {self.http_version}]\n[Status: {self.status}]\n[Explanation: {self.explanation}]\n[Headers: "
+        if len(self.headers) == 0:
+            output += "None"
+        for header in self.headers:
+            output += f"\n\t({header}: {self.headers[header]})"
+        return output + "]"
+
+def stripoutUrl(url: str) -> Url:
     scheme, url = url.split("://", 1)
     assert scheme in ["http", "https"], "Unknown scheme {}".format(scheme)
-    port = 80 if scheme == "http" else 443
 
-    
-    s = socket.socket(
-            family=socket.AF_INET,
-            type=socket.SOCK_STREAM,
-            proto=socket.IPPROTO_TCP,
-        )
-    if url.find("/") != -1:
-        host, path = url.split("/", 1)
-        path = "/" + path
-    else:
-        host = url
-        path = ""
-    if scheme == "https":
-        ctx = ssl.create_default_context()
-        s = ctx.wrap_socket(s, server_hostname=host)
-        url = url = url[len("https://"):]
-    else:
-        url = url[len("http://"):]
+    port = 80 if scheme == "http" else 443
+    host, path = url.split("/", 1)
+    path = "/" + path
     
     if ":" in host:
         host, port = host.split(":", 1)
         port = int(port)
+    return Url(scheme, host, port, path)
     
-    s.connect((host, port))
-    req_header = "GET "+ path +" HTTP/1.0\r\n" + "Host: " + host + "\r\n\r\n"
-    req_header = str.encode(req_header)
-    s.send(req_header)
-    response = s.makefile("r", encoding="latin1", newline="\r\n")
 
+def request(url: Url):
+    # Connect
+    s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
+    s.connect((url.host, url.port))
+    if url.scheme == "https":
+        ctx = ssl.create_default_context()
+        s = ctx.wrap_socket(s, server_hostname=url.host)
+
+    # Request
+    request_path = f"GET {url.path} HTTP/1.0\r\n"
+    request_host = f"Host: {url.host}\r\n\r\n"
+    print(f"{request_path}{request_host}")
+    s.send(f"{request_path}{request_host}".encode("utf8"))
+
+    # Parse Version / Status / Explanation
+    response = s.makefile("r", encoding="latin1", newline="\r\n")
     statusline = response.readline()
     version, status, explanation = statusline.split(" ", 2)
-    assert status == "200", "{}: {}".format(status, explanation)
+    status = int(status)
 
+    # Parse Headers
     headers = {}
     while True:
         line = response.readline()
@@ -56,9 +80,12 @@ def request(url):
         header, value = line.split(":", 1)
         headers[header.lower()] = value.strip()
 
+    # Parse Body
     body = response.read()
     s.close()
-    return headers, body
+
+    return Response(version, status, explanation, headers, body)
+    # return headers, body
 
 def show(body):
     in_angle = False
@@ -72,6 +99,12 @@ def show(body):
 
 if __name__ == "__main__":
     request_url = sys.argv[1]
-    headers, body = request(request_url)
-    print(headers)
-    # show(body)
+    print("Provided URL: %s" %request_url)
+
+    request_url = stripoutUrl(request_url)
+    print("Parsed URL:")
+    print(request_url)
+    print("-"*30)
+
+    response = request(request_url)
+    print(response)
